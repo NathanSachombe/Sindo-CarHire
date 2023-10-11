585fe54e09c1df8ac24203ec7546a7a908b6e7d9
Nathan Guyo Sachombe <nathanguyo6@gmail.com>
2023-10-11 13:15:06 +0300

    Craete HTML dropdown selections

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,19 @@
-// Set source URL
-let url = "https://api.api-ninjas.com/v1/cars?"
+// Ensure the dom is loaded before doing anything
+document.addEventListener('DOMContentLoaded', () => {
+	getCars();
+  
+})
+
 
 // Fetch Request
-fetch("https://api.api-ninjas.com/v1/cars?limit=5&make=audi", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+function getCars(make) {
+    fetch(`https://api.api-ninjas.com/v1/cars?limit=5&make=${make}`,  {
+        method: 'GET',
+        headers: {
+            'X-Api-Key': '8z1iHUxwTH21XU0mSBVpDQ==4047Fe2H0dOtTZWc'
+        },
+        })
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+    }
