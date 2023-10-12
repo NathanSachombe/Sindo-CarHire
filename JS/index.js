0f5f2863f1a59e8e2886967469fd3d2427a06a96
Nathan Guyo Sachombe <nathanguyo6@gmail.com>
2023-10-12 11:50:26 +0300

    Add bootstrap link and script

--- JS/index.js
+++ JS/index.js
@@ -1,41 +1,37 @@
-console.log(1+1);
+console.log(1 + 1);
 
 // Ensure the dom is loaded before doing anything
-document.addEventListener('DOMContentLoaded', () => {
-	getCars();
+document.addEventListener("DOMContentLoaded", () => {
+  getCars();
   //getCars('audi');
-  
-})
+});
 
 // Fetch Request
-function getCars(make = 'toyota') {
-  fetch(`https://api.api-ninjas.com/v1/cars?limit=5&make=${make}`,  {
-      method: 'GET',
-      headers: {
-          'X-Api-Key': '8z1iHUxwTH21XU0mSBVpDQ==4047Fe2H0dOtTZWc'
-      },
-      })
-    .then(response => response.json())
-    .then(result => renderCars(result))
-    .catch(error => console.log('error', error));
-  }
+function getCars(make = "toyota") {
+  fetch(`https://api.api-ninjas.com/v1/cars?limit=5&make=${make}`, {
+    method: "GET",
+    headers: {
+      "X-Api-Key": "8z1iHUxwTH21XU0mSBVpDQ==4047Fe2H0dOtTZWc",
+    },
+  })
+    .then((response) => response.json())
+    .then((result) => renderCars(result))
+    .catch((error) => console.log("error", error));
+}
 
 function renderCars(cars) {
-  const container = document.getElementById('cars-container');
+  const container = document.getElementById("cars-container");
   //const container = document.querySelector('cars-container');
 
   // clear the cars container
-  container.innerHTML = '';
+  container.innerHTML = "";
 
   // iterate through each car and append to the dom
   cars.forEach((car) => {
-    const carName = document.createElement('button');
+    const carName = document.createElement("button");
     carName.textContent = car.model;
-    carName.classList.add(
-      'list-group-item',
-      'list-group-item-action'
-    );
-    carName.addEventListener('click', () => {
+    carName.classList.add("list-group-item", "list-group-item-action");
+    carName.addEventListener("click", () => {
       renderCars(car.model);
     });
 
@@ -44,15 +40,10 @@ function renderCars(cars) {
 }
 
 // Add click event on search button
-function search() {
-  const searchButton = document.getElementById('search');
-  searchButton.addEventListener('click', () => {
-    renderCars(car.model)
-  });
-}
+const searchButton = document.querySelector("#search");
+searchButton.addEventListener("click", () => getCars(textSearch.value));
 
-
-
+// Configure search bar
 const textSearch = document.querySelector("#textSearch");
 textSearch.addEventListener("keyup", (e) => {
   const make = textSearch.value;
@@ -60,8 +51,3 @@ textSearch.addEventListener("keyup", (e) => {
     getCars(make);
   }
 });
-
-
-
-    
-    
